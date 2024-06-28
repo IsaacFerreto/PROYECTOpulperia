27d03954f19faa06adf2aaf07945e4602f5758ea
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-06-28 15:56:43 -0600

    Comenzando a Crear Productos

--- src/Componentes/Producto.jsx
+++ src/Componentes/Producto.jsx
@@ -1,4 +1,19 @@
+import { useEffect} from "react";
+import { useState } from "react";
+import { get } from "../hook/useFecht";
 const Producto =()=>{
+    const [productos,setProductos]=useState([])
+
+    useEffect(()=>{
+        console.log("ENTRA productos");
+        const getUsuario = async()=>{
+            const  dataProductos =await get("products","")
+            setProductos(dataProductos)
+        }
+        getUsuario()
+        console.log(productos);
+    },[])
+
 return(<>
 <div className="card">
   <img src="img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
