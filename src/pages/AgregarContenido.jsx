2bd0648c99c6d8fcba3a8db6e930de7d1b5a395b
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-07-01 23:00:22 -0600

    css a agregarproductos

--- src/pages/AgregarContenido.jsx
+++ src/pages/AgregarContenido.jsx
@@ -2,14 +2,25 @@ const AgregarContenido=()=>{
 
     return(<>
     <h1>Agregar Contenido</h1>
-    <form>
+    <form className="CajaRegistro">
         <div>
         <label htmlFor="nombre">Nombre del producto</label>
         <input type="text" id="nombre"/>
         </div>
-        <div>
-        <label htmlFor="categorias">Categorias</label>
-        <input type="text" id="categorias"/>
+        <div className="categories">
+            <div className="separatioDiv">
+            <form action="/action_page.php" method="post">
+   <fieldset>
+      <legend>Creating checkboxes:</legend>
+      <label for="food1">First food:</label>
+      <input type="checkbox" name="food1" id="food1" value="chicken" /> Chicken
+     <label for="food2">Second food:</label>
+     <input type="checkbox" name="food2" id="food2" value="rice" /> Rice
+     <label for="food3">Third food:</label>
+     <input type="checkbox" name="food3" id="food3" value="potato" /> Potato
+   </fieldset> 
+</form>
+  </div>
         </div>
         <div>
         <label htmlFor="imagen">Imagen del producto</label>
